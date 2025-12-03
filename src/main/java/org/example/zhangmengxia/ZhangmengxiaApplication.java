5383cc2be359654b9b9784fd7425a1c0937f5fca
zhangmx <481956109@qq.com>
2025-12-03 11:55:04 +0800

    first

---
+++ src/main/java/org/example/zhangmengxia/ZhangmengxiaApplication.java
@@ -0,0 +1,13 @@
+package org.example.zhangmengxia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhangmengxiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhangmengxiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/zhangmengxia/ZhangmengxiaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.zhangmengxia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhangmengxiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
